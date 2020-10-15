03f5913b24f2e753e9a02ac7460995842cc9b2fa
MohamedHayBOUALLAGUI <haithem.bouallagui90@gmail.com>
2020-10-16 00:20:12 +0100

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
+import myImage from "./pushurself.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{ border: "1px solid black", maxWidth: "100vw" }}
+        className="container"
+      >
+        <h1 className="title red">
+          We need always positive vibes to be in good mood ^_^ !
+        </h1>
+        <br />
+        <img src={myImage} alt="push_your_self" height="450px" width="700px" />
+        <br />
+        <img src="/success.jpg" alt="success" height="450px" width="700px" />
+      </div>
+      <video width="320" height="240" controls>
+        <source src="/video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
